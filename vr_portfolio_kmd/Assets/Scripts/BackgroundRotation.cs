dcb419a280d4825daf7d3b218b11297d5b2b6bd8
saeron min <saeronmin@saerons-MacBook-Pro.local>
2021-01-30 13:17:20 +0100

    full vr portfolio

---
+++ vr_portfolio_kmd/Assets/Scripts/BackgroundRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRotation : MonoBehaviour
+{
+    public Transform child_background1;
+    public Transform child_background2;
+    public Transform child_background3;
+
+    void LateUpdate()
+    {
+        child_background1.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
+                                                    transform.rotation.eulerAngles.y, 
+                                                    gameObject.transform.rotation.z * -1.0f);
+        child_background2.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
+                                            transform.rotation.eulerAngles.y,
+                                            gameObject.transform.rotation.z * -1.0f);
+        child_background3.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
+                                    transform.rotation.eulerAngles.y,
+                                    gameObject.transform.rotation.z * -1.0f);
+    }
+}
---
+++ vr_portfolio_kmd/Assets/Scripts/FollowUV.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowUV : MonoBehaviour
+{
+    public float parallax = 4f;
+    public float speed = 1f;
+    private float angleXOld;
+    private float angleYOld;
+    //private float time_unit = 0.0;
+
+    void Start()
+    {
+        angleXOld = transform.parent.rotation.eulerAngles.x;
+        angleYOld = transform.parent.rotation.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        Material mat = mr.material;
+        Vector2 offset = mat.mainTextureOffset;
+        //float posx = transform.position.x;
+        //float posy = transform.position.y;
+        float angleX = transform.parent.rotation.eulerAngles.x;
+        float angleY = transform.parent.rotation.eulerAngles.y;
+
+        // Change in angles
+        float changeX = angleX - angleXOld;
+        float changeY = angleY - angleYOld;
+
+        // Only rotation
+        offset.x += changeY / 360f + Time.deltaTime * speed / transform.localScale.x;
+        offset.y -= changeX / 360f + Time.deltaTime * speed / transform.localScale.y;
+        //offset.x = angleY / 360f; 
+        //offset.y = -angleX / 360f;
+
+        // Update old rotations
+        angleXOld = transform.parent.rotation.eulerAngles.x;
+        angleYOld = transform.parent.rotation.eulerAngles.y;
+
+        // No rotation
+        //offset.x = posx;
+        //offset.y = posy;
+        //offset.x += Time.deltaTime / 50f;
+        //offset.x += (posx-offset.x + Time.deltaTime / 50f);
+        //offset.x += Time.deltaTime / 50f + (posx-offset.x); // Change in position += Time.deltaTime/50f;
+        //offset.x = transform.position.x  + Time.deltaTime;
+        mat.mainTextureOffset = offset;
+    }
+}
